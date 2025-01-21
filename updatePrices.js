2f949fe486bd3c7f275059c230ea171a47e6487b
Oliveira Alonso <wnoo@gft.com>
2025-01-21 11:47:09 -0300

    Update prices file

---
+++ updatePrices.js
@@ -0,0 +1,47 @@
+// URL da API para consultar os preços
+const API_URL = 'https://economia.awesomeapi.com.br/json/last/';
+
+// Função para buscar os preços de moedas
+function atualizarPrecos() {
+    precosCriptos.forEach(function(cripto) {
+        getPrecoBRL(cripto.codigoMoeda)
+    });
+}
+
+async function getPrecoBRL(codigoMoeda){
+    console.log('Pegando preco da ==> ', codigoMoeda)
+    const codigoMoedaToBRL = codigoMoeda + '-BRL'
+
+    try {
+        const response = await fetch(`${API_URL}${codigoMoedaToBRL}`);
+        const data = await response.json();
+
+        // Atualiza o valor da Cripto
+        const codeCoinBRL = codigoMoeda + 'BRL'
+
+        console.log('data[COIN] ==> ', data[codeCoinBRL].bid)
+
+        console.log('Atualizando a moeda ', codigoMoeda)
+        let cripto = precosCriptos.find(cripto => cripto.codigoMoeda === codigoMoeda);
+        if(cripto) {
+            cripto.valorAtual = parseFloat(data[codeCoinBRL].bid)
+            console.log('Atualizada com sucesso.')
+        }
+        
+        console.log('')
+        console.log('')
+        console.log('')
+
+        // Após atualizar os preços, verifica os alarmes
+        verificarAlarmes();
+        
+    } catch (error) {
+        console.error('Erro ao atualizar preços:', error);
+    }
+}
+
+// Atualiza os preços a cada minuto
+setInterval(atualizarPrecos, 60000);
+
+// Atualiza os preços ao carregar o script
+atualizarPrecos();
